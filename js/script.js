c817232bccfaac23fbca342b7f64f75afb9f5029
Alexvershilovskiy <99064469+Alexvershilovskiy@users.noreply.github.com>
2022-12-07 12:59:35 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,101 @@
+//бургер
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const body = document.body;
+
+// Клонируем меню, чтобы задать свои стили для мобильной версии
+const menu = document.querySelector("#menu").cloneNode(1);
+
+// При клике на иконку hamb вызываем ф-ию hambHandler
+hamb.addEventListener("click", hambHandler);
+
+// Выполняем действия при клике ..
+function hambHandler(e) {
+  e.preventDefault();
+  // Переключаем стили элементов при клике
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  renderPopup();
+}
+
+// Здесь мы рендерим элементы в наш попап
+function renderPopup() {
+  popup.appendChild(menu);
+}
+
+// Код для закрытия меню при нажатии на ссылку
+const links = Array.from(menu.children);
+
+// Для каждого элемента меню при клике вызываем ф-ию
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+// Закрытие попапа при клике на меню
+function closeOnClick() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+}
+
+
+
+//-- Swiper --
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    430: {
+      slidesPerView: 2,
+      spaceBetween: 5
+    },
+    // when window width is >= 640px
+    767: {
+      slidesPerView: 3,
+      spaceBetween: 5
+    }
+  },
+
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    430: {
+      slidesPerView: 2,
+      spaceBetween: 5
+    },
+    // when window width is >= 640px
+    767: {
+      slidesPerView: 3,
+      spaceBetween: 5
+    }
+  },
+
+  navigation: {
+    nextEl: ".left",
+    prevEl: ".right",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
